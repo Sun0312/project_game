eb6474133c926f9c5d162a84e41ba97d78ab179e
Sun0312 <shb06114@gmail.com>
2021-06-22 02:47:24 +0900

    Add initGame(), runGame()

--- 1.py
+++ 1.py
@@ -1,1 +1,34 @@
-print('This is test file')
+import pygame
+
+# Global variable
+BLACK = (0,0,0)
+pad_width = 480
+pad_height = 640
+
+# main Game start
+
+def runGame():
+    global gamepad, clock
+
+    ongame = False
+    while not ongame:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                doneFlag = True
+
+    gamepad.fill(BLACK)
+    pygame.display.update()
+    clock.tick(60)
+
+# game restart function
+
+def initGame():
+    global gamepad, clock
+
+    pygame.init()
+    gamepad = pygame.display.set_mode((pad_width,pad_height))
+    pygame.display.set_caption('MyGalaga')
+    clock = pygame.time.Clock()
+
+initGame()
+runGame()
